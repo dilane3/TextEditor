6f88dbb7f4194661931c06210df73ca5ebf4fac6
dilane3 <komboudilane125@gmail.com>
2021-01-11 13:41:48 +0100

    edition de l'interface de l'editeur de texte, ajout du style et de la logique pour certaines commandes de base

--- js/command.js
+++ js/command.js
@@ -1,1 +1,39 @@
 // this is the place where we should create all logic
+
+const tableButton = [...document.querySelectorAll("button")];
+
+tableButton.forEach(button => {
+  button.addEventListener('click', function() {
+    let idName = button.id;
+
+    commande(idName);
+  });
+})
+
+function commande(name) {
+  switch (name) {
+    case 'bold':
+      document.execCommand(name, false, null);
+      break;
+
+    case 'italic':
+      document.execCommand(name, false, null);
+      break;
+
+    case 'underline':
+      document.execCommand(name, false, null);
+      break;
+
+    case 'link':
+      let link = prompt("donner le lien");
+
+      if (link !== '') {
+        document.execCommand("createLink", false, link);
+      } else {
+        alert("donner un lien valide");
+      }
+      break;
+
+    default: break;
+  }
+}
