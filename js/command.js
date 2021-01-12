4a4d00548a3baa85583cf683db462b9d5f1e8cd9
dilane3 <komboudilane125@gmail.com>
2021-01-12 21:03:32 +0100

    ajout d'une interface pour l'affichage du texte final

--- js/command.js
+++ js/command.js
@@ -1,6 +1,12 @@
 // this is the place where we should create all logic
 
 const tableButton = [...document.querySelectorAll("button")];
+const result = document.querySelector(".result");
+const submit = document.querySelector(".submit");
+const messageContent = document.querySelector(".editor-body");
+
+
+// la gestion des evenements sur les composants du DOM
 
 tableButton.forEach(button => {
   button.addEventListener('click', function() {
@@ -9,6 +15,15 @@ tableButton.forEach(button => {
     commande(idName);
   });
 })
+
+submit.addEventListener('click', function() {
+  const message = messageContent.innerHTML;
+
+  result.innerHTML = message;
+});
+
+
+// la definition des fonctions
 
 function commande(name) {
   switch (name) {
